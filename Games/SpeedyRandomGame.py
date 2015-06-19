bb28a318656d167ea6d49e1559cf274fb7591248
Jim W <seekatar@gmail.com>
2015-06-19 17:46:09 -0400

    game and hardware fixes

---
+++ Games/SpeedyRandomGame.py
@@ -0,0 +1,55 @@
+import Base
+import datetime
+import random
+from FixedRandomGame import FixedRandomGame as __base
+
+class SpeedyRandomGame(__base):
+    """
+    Game with a declining speed to hit the plates
+    """
+    
+    def __init__(self):
+        super(SpeedyRandomGame,self).__init__()
+        self.set_names('Speedy touch game',
+                          'Faster!',
+                          'Jim Wallace',
+                          datetime.date(2015,6,14),
+                          '0.1')
+        self.LOOP_CNT = 100
+        self._timeout_sec = 2
+        self._interval_sec = 2
+
+
+    def get_timeout_sec(self):
+        """
+        override to adjust timeout each time they touch
+        """
+        self._interval_sec *= .95
+        self.hardware.write_debug("now",self._interval_sec,self.LOOP_CNT)
+        return self._interval_sec
+    
+    def _timeout(self,button):
+        """
+        return True to end the game
+        """
+        return True
+    
+    def _hit(self,button):
+        """
+        return True to end the game
+        """
+        self._score += 1
+        self.hardware.light_good(.2)\
+                     .display_number(self._score)
+        
+        return False
+
+    def _miss(self,button,missed_button):
+        """
+        return True to end the game
+        """
+        self.hardware.light_bad()\
+                .beep(duration_sec=.2)\
+                .light_on(button)
+        
+        return False
